e88508cc127527fac555a41f54063220be4ba7e9
kaungkhantthar7777 <kaungkhantthar@ucsm.edu.mm>
2020-06-08 15:11:48 +0630

    Updates

---
+++ precache-manifest.d431fcc5c8dc4ab36f6bfcde6fe95a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d142f02fb2bb1540d482ce2a264423",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aca4ee531238f2377b6e",
+    "url": "/static/css/main.1e59e1f0.chunk.css"
+  },
+  {
+    "revision": "551a1eb818f54c06c689",
+    "url": "/static/js/2.b75d4539.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b75d4539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca4ee531238f2377b6e",
+    "url": "/static/js/main.3b26ebbe.chunk.js"
+  },
+  {
+    "revision": "13746591cb5183a413cc",
+    "url": "/static/js/runtime-main.590acf33.js"
+  }
+]);
